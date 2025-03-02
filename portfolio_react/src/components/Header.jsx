a52df444c1335532a820caab4afc6c00500dfd6a
Lolopiz <[lorenzi.matteo30@gmail.com]>
2025-03-02 16:11:05 +0100

    update Header logo text; add disabled state for project source links and improve project routing

--- portfolio_react/src/components/Header.jsx
+++ portfolio_react/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <header className="header">
-      <div className="logo">Matteo</div>
+      <div className="logo">Portfolio</div>
       <nav>
         <ul>
           <li><Link to="/">Accueil</Link></li>
--- portfolio_react/src/components/Projects.jsx
+++ portfolio_react/src/components/Projects.jsx
@@ -26,8 +26,8 @@ const Projects = () => {
             <h3>Terraforma</h3>
             <p>Création d’un jeu vidéo ludo-éducatif pour un niveau de 3ème en SVT.</p>
             <div className="project-links">
-              <Link to="/all-projects" className="project-link primary">Voir le projet</Link>
-              <a href="#" className="project-link secondary">Code source</a>
+              <Link to="/projects/1" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
             </div>
           </div>
         </div>
@@ -46,8 +46,8 @@ const Projects = () => {
             <h3>GameJam</h3>
             <p>Création d’un jeu en lien avec l’IUT.</p>
             <div className="project-links">
-              <Link to="/all-projects" className="project-link primary">Voir le projet</Link>
-              <a href="#" className="project-link secondary">Code source</a>
+              <Link to="/projects/2" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
             </div>
           </div>
         </div>
@@ -66,80 +66,78 @@ const Projects = () => {
             <h3>Fish’Event</h3>
             <p>Développement d'une application évènementielle pour la pêche.</p>
             <div className="project-links">
-              <Link to="/all-projects" className="project-link primary">Voir le projet</Link>
-              <a href="#" className="project-link secondary">Code source</a>
+              <Link to="/projects/3" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
             </div>
           </div>
         </div>
 
         {/* Projet 4: Comparaison d’approches algorithmiques */}
         {/*
-  <div className="project-card">
-    <div className="project-image">
-      <div className="project-overlay">
-        <div className="project-tags">
-          <span>Algorithmie</span>
-          <span>Code</span>
+        <div className="project-card">
+          <div className="project-image">
+            <div className="project-overlay">
+              <div className="project-tags">
+                <span>Algorithmie</span>
+                <span>Code</span>
+              </div>
+            </div>
+          </div>
+          <div className="project-content">
+            <h3>Comparaison d’approches algorithmiques</h3>
+            <p>Comparaison d’approches algorithmiques.</p>
+            <div className="project-links">
+              <Link to="/projects/4" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="project-content">
-      <h3>Comparaison d’approches algorithmiques</h3>
-      <p>Comparaison d’approches algorithmiques.</p>
-      <div className="project-links">
-        <a href="#" className="project-link primary">Voir le projet</a>
-        <a href="#" className="project-link secondary">Code source</a>
-      </div>
-    </div>
-  </div>
-  */}
+        */}
 
         {/* Projet 5: Site web pour élèves de 3ème */}
         {/*
-  <div className="project-card">
-    <div className="project-image">
-      <div className="project-overlay">
-        <div className="project-tags">
-          <span>Web</span>
-          <span>Éducation</span>
+        <div className="project-card">
+          <div className="project-image">
+            <div className="project-overlay">
+              <div className="project-tags">
+                <span>Web</span>
+                <span>Éducation</span>
+              </div>
+            </div>
+          </div>
+          <div className="project-content">
+            <h3>Site web pour élèves de 3ème</h3>
+            <p>Site web simplifiant la recherche de stage pour les élèves de 3ème.</p>
+            <div className="project-links">
+              <Link to="/projects/5" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="project-content">
-      <h3>Site web pour élèves de 3ème</h3>
-      <p>Site web simplifiant la recherche de stage pour les élèves de 3ème.</p>
-      <div className="project-links">
-        <a href="#" className="project-link primary">Voir le projet</a>
-        <a href="#" className="project-link secondary">Code source</a>
-      </div>
-    </div>
-  </div>
-  */}
+        */}
 
         {/* Projet 6: Installation d'un poste pour le développement */}
         {/*
-  <div className="project-card">
-    <div className="project-image">
-      <div className="project-overlay">
-        <div className="project-tags">
-          <span>Développement</span>
-          <span>Installation</span>
+        <div className="project-card">
+          <div className="project-image">
+            <div className="project-overlay">
+              <div className="project-tags">
+                <span>Développement</span>
+                <span>Installation</span>
+              </div>
+            </div>
+          </div>
+          <div className="project-content">
+            <h3>Installation d'un poste pour le développement</h3>
+            <p>Installation d'un poste pour le développement.</p>
+            <div className="project-links">
+              <Link to="/projects/6" className="project-link primary">Voir le projet</Link>
+              <a href="#" className="project-link secondary disabled">Code source</a>
+            </div>
+          </div>
         </div>
+        */}
       </div>
-    </div>
-    <div className="project-content">
-      <h3>Installation d'un poste pour le développement</h3>
-      <p>Installation d'un poste pour le développement.</p>
-      <div className="project-links">
-        <a href="#" className="project-link primary">Voir le projet</a>
-        <a href="#" className="project-link secondary">Code source</a>
-      </div>
-    </div>
-  </div>
-  */}
-      </div>
-
-
 
       <div className="projects-cta">
         <Link to="/all-projects" className="btn primary">Voir tous mes projets</Link>
